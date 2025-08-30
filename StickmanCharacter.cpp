9e16b95070233414e6f57d5d62c610e8d16d378d
sehajpandal01 <36576068+sehajpandal01@users.noreply.github.com>
2025-08-29 23:16:47 -0700

    Update StickmanCharacter.cpp

--- StickmanCharacter.cpp
+++ StickmanCharacter.cpp
@@ -22,7 +22,7 @@ AStickmanCharacter::AStickmanCharacter()
     SetupStickmanMesh();
     SetupAudioComponent();
 
-    // Ca
+    // C
     CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
     CameraBoom->SetupAttachment(RootComponent);
     CameraBoom->TargetArmLength = 350.f;
