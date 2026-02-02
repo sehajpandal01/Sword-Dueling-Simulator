63bae76cfa011c784b2d8ea8e609652ef1986a50
sehajpandal01 <36576068+sehajpandal01@users.noreply.github.com>
2026-02-02 00:14:09 -0800

    Remove blank line in StickmanCharacter.h
    Removed a blank line for better code formatting.

--- StickmanCharacter.h
+++ StickmanCharacter.h
@@ -50,7 +50,6 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Audio") class UAudioComponent* AudioComponent;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true")) class USpringArmComponent* CameraBoom;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true")) class UCameraComponent* FollowCamera;
-
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats") int32 STR=5, DEX=5, END=5, VIT=5, FOC=5;
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Stats") float MaxStamina=100.f;
     UPROPERTY(BlueprintReadOnly, Category="Stats") float Stamina=100.f;
